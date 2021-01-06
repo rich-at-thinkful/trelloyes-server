88de493b300be92ecb651740eece20f67ea7702a
Rich Greenhill <rgreenhill@thinkful.com>
2021-01-06 07:35:30 -0800

    refactored routes complete

---
+++ src/dataStore.js
@@ -0,0 +1,16 @@
+const cards = [{
+  id: '8c4ab8ff-85d8-4464-a02f-f584fd7ecd06',
+  title: 'Task One',
+  content: 'This is card one'
+}];
+
+const lists = [{
+  id: 'd9a31315-8238-4eb5-aed9-1a86d1d835d5',
+  header: 'List One',
+  cardIds: [1]
+}];
+
+module.exports = {
+  cards,
+  lists,
+};
